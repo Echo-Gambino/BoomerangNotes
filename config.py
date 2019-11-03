c5adcccfd7baa1a4f60160e1f157d0333ed04131
Echo-Gambino <nkb012pomm@gmail.com>
2019-11-03 13:12:24 -0700

    Added automatic text editor switching depending on what operating system you are using.

--- config.py
+++ config.py
@@ -1,5 +1,21 @@
+from sys import platform
 import os
+
+
+LINUX_EDITOR = "kate"
+MACOS_EDITOR = "TextEdit"
+WNDOS_EDITOR = "notepad.exe"
+
+TEXT_EDITOR_DICTIONARY = {
+    'linux': LINUX_EDITOR,
+    'linux2': LINUX_EDITOR,
+    'darwin': MACOS_EDITOR,
+    'win32': WNDOS_EDITOR
+}
+
+
 working_dir = os.path.dirname(os.path.realpath(__file__))
+text_editor = TEXT_EDITOR_DICTIONARY[platform]
 
 
 class Config:
@@ -13,7 +29,7 @@ class Config:
 
     TEMP_PATH = WORKING_DIR + '/data/temp/tmp.txt'
 
-    TEXT_EDITOR = 'kate'
+    TEXT_EDITOR = text_editor
 
     def get_reminder_path(self, title):
         output = '{0}/data/{1}_Reminder.html'.format(self.WORKING_DIR, title)
